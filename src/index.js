d4945383d9c47e527084e339f1215ddf141b5ac6
A L V A R O <alvarop3000@gmail.com>
2020-08-06 18:04:32 -0500

    add weather icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const img = document.querySelector('img');
+
 function setCity(city){
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=886700da32110e4284daaccee7bdf897`, {mode: 'cors'})
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data.main))
+  .then(srce => img.src = srce.data.images.original.url)
 };
+
+setCity('london');
