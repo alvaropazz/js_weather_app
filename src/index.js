2af77bfa1171c10ac8f22f95e18f2b8bd8e2959c
A L V A R O <alvarop3000@gmail.com>
2020-08-07 17:42:06 -0500

    correct linters from first version

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import './styles.css';
 
-let weatherIcon = document.querySelector('.weather-icon');
-let weatherDescription = document.querySelector('.description')
-let temperatureBox = document.querySelector('.temperature')
-let weatherLocation = document.querySelector('.location')
+const weatherIcon = document.querySelector('.weather-icon');
+const weatherDescription = document.querySelector('.description');
+const temperatureBox = document.querySelector('.temperature');
+const weatherLocation = document.querySelector('.location');
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-let dateObj = new Date();
-let month = monthNames[dateObj.getMonth()];
-let day = String(dateObj.getDate()).padStart(2, '0');
-let year = dateObj.getFullYear();
-let output = month  + ' '+ day + ', ' + year;
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'];
+const dateObj = new Date();
+const month = monthNames[dateObj.getMonth()];
+const day = String(dateObj.getDate()).padStart(2, '0');
+const year = dateObj.getFullYear();
+const output = `${month} ${day}, ${year}`;
 
 document.querySelector('.date').innerHTML = output;
 
-async function setter(city){
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=886700da32110e4284daaccee7bdf897`, {mode: 'cors'});
+async function setter(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=886700da32110e4284daaccee7bdf897`, { mode: 'cors' });
   const json = await response.json();
-  return json
-};
+  return json;
+}
 
-let setLocation = document.querySelector('.search')
-setLocation.addEventListener('submit', function(e){
-  e.preventDefault()
-  let form = e.target
-  let place = form.querySelector('input[name="place"]').value
+const setLocation = document.querySelector('.search');
+setLocation.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const place = form.querySelector('input[name="place"]').value;
 
   setter(place).then(data => {
     weatherIcon.src = `../src/assets/icons/${data.weather[0].icon}.png`;
     weatherDescription.innerHTML = data.weather[0].description;
-    temperatureBox.innerHTML = data.main.temp+'&#xb0';
-    weatherLocation.innerHTML = data.name + ', ' + data.sys.country;
+    temperatureBox.innerHTML = `${data.main.temp}&#xb0`;
+    weatherLocation.innerHTML = `${data.name}, ${data.sys.country}`;
   });
-
-})
+});
