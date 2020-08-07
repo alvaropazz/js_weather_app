0382d56c08c9a310a2b4399f583b83f18b735a50
A L V A R O <alvarop3000@gmail.com>
2020-08-07 17:13:39 -0500

    added main functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import './styles.css';
 
-const img = document.querySelector('img');
+let weatherIcon = document.querySelector('.weather-icon');
+let weatherDescription = document.querySelector('.description')
+let temperatureBox = document.querySelector('.temperature')
+let weatherLocation = document.querySelector('.location')
 
-function setCity(city){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=886700da32110e4284daaccee7bdf897`, {mode: 'cors'})
-  .then(response => response.json())
-  .then(data => console.log(data.main))
-  .then(srce => img.src = srce.data.images.original.url)
+const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+let dateObj = new Date();
+let month = monthNames[dateObj.getMonth()];
+let day = String(dateObj.getDate()).padStart(2, '0');
+let year = dateObj.getFullYear();
+let output = month  + ' '+ day + ', ' + year;
+
+document.querySelector('.date').innerHTML = output;
+
+async function setter(city){
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=886700da32110e4284daaccee7bdf897`, {mode: 'cors'});
+  const json = await response.json();
+  return json
 };
 
-setCity('london');
+let setLocation = document.querySelector('.search')
+setLocation.addEventListener('submit', function(e){
+  e.preventDefault()
+  let form = e.target
+  let place = form.querySelector('input[name="place"]').value
+
+  setter(place).then(data => {
+    weatherIcon.src = `../src/assets/icons/${data.weather[0].icon}.png`;
+    weatherDescription.innerHTML = data.weather[0].description;
+    temperatureBox.innerHTML = data.main.temp+'&#xb0';
+    weatherLocation.innerHTML = data.name + ', ' + data.sys.country;
+  });
+
+})
+
+
+  
