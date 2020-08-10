31f9d1e8be2e1b027d717803353c712a6b0f0f10
A L V A R O <alvarop3000@gmail.com>
2020-08-10 16:15:13 -0500

    add local time

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ const weatherDescription = document.querySelector('.description');
 const temperatureBox = document.querySelector('.temperature');
 const weatherLocation = document.querySelector('.location');
 
-const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'];
-const dateObj = new Date();
-const month = monthNames[dateObj.getMonth()];
-const day = String(dateObj.getDate()).padStart(2, '0');
-const year = dateObj.getFullYear();
-const output = `${month} ${day}, ${year}`;
-
-document.querySelector('.date').innerHTML = output;
-
 async function setter(city, unit) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=886700da32110e4284daaccee7bdf897`, { mode: 'cors' });
   const json = await response.json();
@@ -32,6 +22,16 @@ setLocation.addEventListener('submit', (e) => {
     weatherIcon.src = `../src/assets/icons/${data.weather[0].icon}.png`;
     weatherDescription.innerHTML = data.weather[0].description;
     temperatureBox.innerHTML = `${data.main.temp}&#xb0`;
-    weatherLocation.innerHTML = `${data.name}, ${data.sys.country}`;
+    weatherLocation.innerHTML = calcTime(data.name, data.timezone);
+    ;
   });
+
+
 });
+
+function calcTime(city, offset) {
+  let date = new Date();
+  let utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
+  let nd = new Date(utc + (1000 * offset));
+  return city + "'s date is "+ nd.toLocaleString();
+}
