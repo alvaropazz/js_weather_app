e0d95a5b567630e8051daf3f71d11222540214c9
A L V A R O <alvarop3000@gmail.com>
2020-08-10 18:04:28 -0500

    bundle images

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import './styles.css';
 import getDate from './modules/date';
 import setter from './modules/setter';
-import o1d from './assets/icons/01d.png';
-import o1n from './assets/icons/01d.png';
-import o2d from './assets/icons/02d.png';
-import o2n from './assets/icons/02n.png';
-import o3d from './assets/icons/03d.png';
-import o3n from './assets/icons/03n.png';
-import o4d from './assets/icons/04d.png';
-import o4n from './assets/icons/04n.png';
-import o9d from './assets/icons/09d.png';
-import o9n from './assets/icons/09n.png';
-import o10d from './assets/icons/10d.png';
-import o10n from './assets/icons/10n.png';
-import o11d from './assets/icons/11d.png';
-import o11n from './assets/icons/11n.png';
-import o13d from './assets/icons/13d.png';
-import o13n from './assets/icons/13n.png';
-import o50d from './assets/icons/50d.png';
-import o50n from './assets/icons/50n.png';
+import a_01d from './assets/icons/a_01d.png';
+import a_01n from './assets/icons/a_01n.png';
+import a_02d from './assets/icons/a_02d.png';
+import a_02n from './assets/icons/a_02n.png';
+import a_03d from './assets/icons/a_03d.png';
+import a_03n from './assets/icons/a_03n.png';
+import a_04d from './assets/icons/a_04d.png';
+import a_04n from './assets/icons/a_04n.png';
+import a_09d from './assets/icons/a_09d.png';
+import a_09n from './assets/icons/a_09n.png';
+import a_10d from './assets/icons/a_10d.png';
+import a_10n from './assets/icons/a_10n.png';
+import a_11d from './assets/icons/a_11d.png';
+import a_11n from './assets/icons/a_11n.png';
+import a_13d from './assets/icons/a_13d.png';
+import a_13n from './assets/icons/a_13n.png';
+import a_50d from './assets/icons/a_50d.png';
+import a_50n from './assets/icons/a_50n.png';
 
 const weatherIcon = document.querySelector('.weather-icon');
 const weatherDescription = document.querySelector('.description');
@@ -36,7 +36,7 @@ setLocation.addEventListener('submit', (e) => {
     if (data.cod === '404') {
       alert('no data for input location'); // eslint-disable-line no-alert
     } else {
-      weatherIcon.src = `../src/assets/icons/${data.weather[0].icon}.png`;
+      weatherIcon.src = `a_${data.weather[0].icon}`;
       weatherDescription.innerHTML = data.weather[0].description;
       temperatureBox.innerHTML = `${data.main.temp}&#xb0`;
       weatherLocation.innerHTML = getDate(data.name, data.timezone);
